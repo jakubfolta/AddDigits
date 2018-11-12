5cae5d728292749356596010cdaa05490b23abbd
Jakub Folta <jakubfolta@yahoo.co.uk>
2018-11-12 21:32:46 +0100

    solidifying knowledge

--- Add.py
+++ Add.py
@@ -6,10 +6,13 @@ def adding(number):
 
 print(adding(1235678))
 
+
+
 def adding(number):
   return sum(int(num) for num in str(number))
 
 print(adding(3446722))
+
 
 def add(digits):
   total = 0
@@ -18,3 +21,12 @@ def add(digits):
   return total
 
 print(add(1222345))
+
+
+def adding(numbers):
+  numbersGroup = []
+  for digit in str(numbers):
+    numbersGroup.append(int(digit))
+  return sum(numbersGroup)
+
+print(adding(11234))
