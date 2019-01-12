2d97005af6901e70470e6167069d88ba8c146305
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-12 21:42:38 +0100

    solidifying knowledge

--- AddNumb.py
+++ AddNumb.py
@@ -11,3 +11,19 @@ def adding(numbers):
   return total
 
 print(adding(9876))
+
+def addNumbers(numbers):
+  total = 0
+  for x in str(numbers):
+    total += int(x)
+  return total
+
+print(addNumbers(423566))
+
+def addNumbersFromList(numbers):
+  total = 0
+  for x in numbers:
+    total += x
+  return total
+
+print(addNumbersFromList([3, 4, 6, 4, 2, 2, 3]))
