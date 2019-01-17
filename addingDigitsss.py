2bcb47d1464e13bc0b31ca170f9c7c82d6d0697c
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-01-17 21:00:56 +0100

    solidifying knowledge

--- addingDigitsss.py
+++ addingDigitsss.py
@@ -18,3 +18,11 @@ def adding(number):
     return sum([int(x) for x in str(number)])
 
 print(adding(5442354556))
+
+def addDigits(number):
+    total = 0
+    for x in str(number):
+        total += int(x)
+    return total
+
+print(addDigits(1112))
