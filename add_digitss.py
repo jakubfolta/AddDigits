de0c4a7eecbabd2dffdb8181c0acef54aae0bf58
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-03-25 11:52:31 +0100

    continuation

--- add_digitss.py
+++ add_digitss.py
@@ -2,3 +2,11 @@ def add_digits(numbers):
     return (sum([int(x) for x in list(str(numbers))]))
 
 print(add_digits(432565))
+
+def add_digits(digits):
+    total = []
+    for x in str(digits):
+        total.append(int(x))
+    
+
+add_digits(424455676578)
