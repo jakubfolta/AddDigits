3e739287ad39b093cafae0e281fdc103f1806f39
Jakub Folta <jakubfolta@yahoo.co.uk>
2019-04-21 15:01:42 +0200

    solidifying knowledge

--- add_dig_solidifying.py
+++ add_dig_solidifying.py
@@ -10,3 +10,11 @@ def add(numbers):
     return sum([int(x) for x in str(numbers)])
 
 print(add(32446))
+
+def add_digits(numbers):
+    numbers_list = []
+    for x in str(numbers):
+        numbers_list.append(int(x))
+    return sum(numbers_list)
+
+print(add_digits(12314))
